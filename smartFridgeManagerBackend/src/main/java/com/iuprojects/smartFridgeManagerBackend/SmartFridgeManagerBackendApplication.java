1d100d82929caf716f59f5285211f9ab7787e5c4
Theresa Duennebeil <theresa.duennebeil@yahoo.de>
2023-08-31 16:53:23 +0200

    added project from spring initializr

---
+++ smartFridgeManagerBackend/src/main/java/com/iuprojects/smartFridgeManagerBackend/SmartFridgeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iuprojects.smartFridgeManagerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFridgeManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFridgeManagerBackendApplication.class, args);
+	}
+
+}
---
+++ smartFridgeManagerBackend/src/test/java/com/iuprojects/smartFridgeManagerBackend/SmartFridgeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuprojects.smartFridgeManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFridgeManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
