4707f7c1dd32d7e2a70855dd75d0e7f74318c0e0
Theresa Duennebeil <theresa.duennebeil@yahoo.de>
2023-09-03 17:18:26 +0200

    setup vue project

---
+++ smartFridgeManagerFrontend/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TestComponent from './TestComponent.vue';
+
+defineProps<{
+  msg: string
+}>()
+
+</script>
+
+<template>
+  <TestComponent />
+</template>
+
+<style scoped></style>
